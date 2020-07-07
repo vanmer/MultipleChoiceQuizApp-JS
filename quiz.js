ebef9229a0709f0f94ee3fcf796f6e0013f0ac9c
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-07 22:58:51 +0200

    select elements, create questions

---
+++ quiz.js
@@ -0,0 +1,40 @@
+// SELECT THE ELEMENTS
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("score");
+
+// CREATE QUESTIONS
+let questions = [
+  {
+      question: "What does HTML stand for",
+      imgSrc: "img/html.png",
+      choiceA: "Correct",
+      choiceB: "Wrong",
+      choiceC: "Wrong",
+      correct: "A"
+  },
+  {
+      question: "What does CSS stand for",
+      imgSrc: "img/css.png",
+      choiceA: "Wrong",
+      choiceB: "Correct",
+      choiceC: "Wrong",
+      correct: "B"
+  },
+  {
+      question: "What does JS stand for",
+      imgSrc: "img/js.png",
+      choiceA: "Wrong",
+      choiceB: "Wrong",
+      choiceC: "Correct",
+      correct: "C"
+  }
+];
